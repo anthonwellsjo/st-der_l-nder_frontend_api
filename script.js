118b7043b730f538d33c0252c9c6c46d91d8f8ea
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-09-29 11:08:35 +0200

    With menues and transitions
    Added follow functions:
    
    *Working dynamic country and city menues
    *City info modal
    *Transition for city info modal
    *Dynamic info in city modal

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.body.onload = loadApp;
 async function loadApp() {
     await appData.get();
     const app = document.getElementById("app");
+    appData
     page.appendChild(page.create(), app);
+    page.renderCountryList();
 
 }
 
@@ -18,6 +20,9 @@ const page = {
     create: function () {
         return (
             `<div id="pageHolder">
+                    <div id="modalHolder">
+                        
+                    </div>
                 <div id="frame">
                     <header id="header">
                         <h1>${page.pageName}</h1>
@@ -25,8 +30,7 @@ const page = {
                     <div id="sidebar">
                         <div id="countryNavHolder">
                             <nav id="countryNav">
-                                <ul id="countryOl">
-                                ${appData.renderCountryList()}
+                                <ul id="countryUl">
                                 </ul>
                             </nav>
                         </div>
@@ -34,8 +38,7 @@ const page = {
                     <div id="centerPage">
                         <div id="cityListHolder">
                             <div id="cityListContainer">
-                                <ul id="cityList">
-                                ${appData.renderCityList()}
+                                <ul id="cityUl">
                                 </ul>
                             </div>
                         </div>
@@ -48,9 +51,74 @@ const page = {
         parent.innerHTML = child;
         return;
     },
+    renderCountryList: () => {
+        appData.countries.forEach(country => {
+            console.log(country)
+            let li = document.createElement("li");
+            li.setAttribute("class", "countryListItem");
+            li.addEventListener("click", (e) => eventHandlers.onCountryClickEventHandler(e))
+            li.innerHTML = country.countryname;
+            document.getElementById("countryUl").appendChild(li);
+        })
+    },
+    renderCityList: () => {
+        if (page.currentCountry !== page.currentlyRenderedCountryCityList) {
+            console.log("rendering city list");
+            document.getElementById("cityUl").innerHTML = "";
+            const citiesToRender = appData.cities.filter(cit => cit.countryid === page.currentCountry.id);
+            citiesToRender.forEach(city => {
+                let li = document.createElement("li");
+                li.setAttribute("class", "cityListItem");
+                li.addEventListener("click", (e) => eventHandlers.onCityClickEventHandler(e))
+                li.innerHTML = city.stadname;
+                document.getElementById("cityUl").appendChild(li);
+            })
+        }
+    },
+    renderCityModalPage: () => {
+
+        //Skapar modal (är gömd för tillfället med css: translateY)
+        document.getElementById("modalHolder").innerHTML = `
+                        <div class="cityModal">
+                            <div id="modalWindowHeader">
+                                <div id="modalCloseBtnHolder">
+                                    <button id="closeModalBtn">X</button>
+                                </div>
+                            </div>
+                            <div id="modalContainer">
+                                <header id="modalHeader">
+                                </header>
+                                <content id="modalContent">
+                                </content
+                            </div>
+                        </div>
+        `
+        //Lägg till header dynamiskt
+
+        let header = document.createElement("h2");
+        header.innerText = page.currentCity.stadname;
+        document.getElementById("modalHeader").appendChild(header);
+
+        //Lägg till content dynamiskt
+
+        let content = document.createElement("p");
+        content.innerText = `
+        ${page.currentCity.stadname} är en stad som ligger i ${page.currentCountry.countryname} och har ${page.currentCity.population} invånare.
+        `
+
+        //Lägg till class på modalContent som gör transition med translateY (med timeout annars blir det ingen animation)
+        document.getElementById("modalContent").appendChild(content);
+        setTimeout(() => { document.getElementsByClassName("cityModal")[0].classList.add("open"); }, 10)
+
+        //Skapa event hanlder på stäng-knapp
+        document.getElementById("closeModalBtn").addEventListener("click", eventHandlers.onModalBtnClosedClickedEventHandler)
+
+
+    },
     pageName: "Travel Partner",
-
-
+    currentCountry: null,
+    currentlyRenderedCountryCityList: null,
+    currentCity: null
 }
 
 const appData = {
@@ -78,21 +146,23 @@ const appData = {
         appData.cities = cityData;
         appData.hasBeenFetched = true;
     }),
-    renderCountryList: () => {
-        let htmlString = "";
-        appData.countries.forEach(country => {
-            htmlString+=`<li class="countryListObject">${country.countryname}</li>`
-        })
-        return htmlString;
-    },
-    renderCityList: () => {
-        let htmlString = "";
-        appData.cities.forEach(city => {
-            
-        })
-        return "<li>Test Stad</li>"
-    },
     hasBeenFetched: false,
     cities: {},
     countries: {}
+}
+
+
+const eventHandlers = {
+    onCountryClickEventHandler: e => {
+        page.currentCountry = appData.countries.find(c => c.countryname === e.target.innerHTML);
+        page.renderCityList();
+    },
+    onCityClickEventHandler: e => {
+        page.currentCity = appData.cities.find(c => c.stadname == e.target.innerHTML);
+        page.renderCityModalPage();
+        console.log("curr city", page.currentCity);
+    },
+    onModalBtnClosedClickedEventHandler: () => {
+        document.getElementsByClassName("cityModal")[0].classList.remove("open"); 
+    }
 }
