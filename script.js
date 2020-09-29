1e13fad01d36a6427b191095e62c6ca9db4f0a72
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-09-29 22:58:22 +0200

    quick commit children crying

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.body.onload = loadApp;
 async function loadApp() {
     await appData.get();
     const app = document.getElementById("app");
-    appData
     page.appendChild(page.create(), app);
     page.renderCountryList();
     page.createUnchangingEventHandlers();
@@ -30,10 +29,13 @@ const page = {
                         <h1>${page.pageName}</h1>
                     </header>
                     <div id="sidebar">
+                        <div id="sidebarVerticalBorder">
+                        </div>
                         <header id="countryHeader">
                             <h2>
                                 Countries
                             </h2>
+                            <div id="headerBottomBorder"></div>
                         </header>
                         <div id="countryNavHolder">
                             <nav id="countryNav">
@@ -43,10 +45,8 @@ const page = {
                         </div>
                     </div>
                     <div id="centerPage">
-                        <header id="cityHeader>
-                            <h2>
-                                Cities
-                            </h2>
+                        <header id="cityHeader">
+                            <h2 id="cityHeaderH2">Cities</h2>
                         </header>
                         <div id="cityListHolder">
                             <div id="cityListContainer">
@@ -80,11 +80,13 @@ const page = {
             const citiesToRender = appData.cities.filter(cit => cit.countryid === page.currentCountry.id);
             citiesToRender.forEach(city => {
                 let li = document.createElement("li");
-                li.setAttribute("class", "cityListItem");
+                li.setAttribute("class", "cityListItem")
                 li.addEventListener("click", (e) => eventHandlers.onCityClickEventHandler(e))
                 li.innerHTML = city.stadname;
                 document.getElementById("cityUl").appendChild(li);
             })
+            page.allVisibleListItemsShowing = false;
+            setTimeout(() => { page.toggleShowListItems(); }, 10);
         }
     },
     renderCityModalPage: () => {
@@ -157,15 +159,62 @@ const page = {
     createUnchangingEventHandlers: () => {
         //Backdrop click
         document.getElementsByClassName("backDrop")[0].addEventListener("click", eventHandlers.onBackDropClicked);
+
+        //Country Click
+        document.getElementById("countryHeader").addEventListener("click", eventHandlers.onCountriesHeaderClicked)
     },
     closeModal: () => {
         document.getElementsByClassName("cityModal")[0].classList.remove("open");
         page.hideBackDrop();
     },
+    disactiveCountryListItems: () => {
+        const lItems = document.getElementsByClassName("countryListItem");
+        Object.keys(lItems).forEach(key => {
+            lItems[key].classList.remove("active");
+        })
+    },
+    disactiveCityListItems: () => {
+        const lItems = document.getElementsByClassName("cityListItem");
+        Object.keys(lItems).forEach(key => {
+            lItems[key].classList.remove("active");
+        })
+    },
+    toggleShowListItems: () => {
+        let listItems = document.getElementsByTagName("li");
+        if (!page.allVisibleListItemsShowing) {
+            Object.keys(listItems).forEach(key => {
+                listItems[key].classList.add("show");
+            })
+            page.allVisibleListItemsShowing = true;
+        } else {
+            Object.keys(listItems).forEach(key => {
+                listItems[key].classList.remove("show");
+            })
+            page.allVisibleListItemsShowing = false;
+        }
+    },
+    toggleShowCitiesHeader: () => {
+        if (!page.citiesHeaderShowing) {
+            document.getElementById("cityHeaderH2").classList.add("show");
+            page.citiesHeaderShowing = true;
+        } else {
+            document.getElementById("cityHeaderH2").classList.remove("show");
+            page.citiesHeaderShowing = false;
+        }
+    },
+    toggleShowBorders: () => {
+        if (!page.bordersShowing) {
+            document.getElementById("sidebarVerticalBorder").setAttribute("class", "show");
+
+        }
+    },
     pageName: "Travel Partner",
     currentCountry: null,
     currentlyRenderedCountryCityList: null,
-    currentCity: null
+    currentCity: null,
+    allVisibleListItemsShowing: false,
+    citiesHeaderShowing: false,
+    bordersShowing: false,
 }
 
 const appData = {
@@ -202,15 +251,19 @@ const appData = {
 
 const eventHandlers = {
     onCountryClickEventHandler: e => {
+        page.disactiveCountryListItems();
+        e.target.classList.add("active");
         page.currentCountry = appData.countries.find(c => c.countryname === e.target.innerHTML);
         page.renderCityList();
     },
     onCityClickEventHandler: e => {
+        e.target.classList.add("active");
         page.currentCity = appData.cities.find(c => c.stadname == e.target.innerHTML);
         page.renderCityModalPage();
         console.log("curr city", page.currentCity);
     },
     onModalBtnClosedClickedEventHandler: () => {
+        page.disactiveCityListItems();
         page.closeModal();
 
     },
@@ -220,10 +273,15 @@ const eventHandlers = {
         } else {
             appData.citiesVisited = appData.citiesVisited.filter(c => c !== page.currentCity.id)
         }
-
         page.renderVisitedButton();
     },
     onBackDropClicked: () => {
+        page.disactiveCityListItems();
         page.closeModal();
+    },
+    onCountriesHeaderClicked: () => {
+        page.toggleShowListItems();
+        page.toggleShowCitiesHeader();
+        page.toggleShowBorders();
     }
 }
